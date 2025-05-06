ae450c3f32db85e0cdaf03a319b46f5d67e66577
Mahidul Islam <mahidulislamtamim@gmail.com>
2025-05-06 11:33:32 +0600

    PDF to Docx converter

---
+++ main.py
@@ -0,0 +1,20 @@
+# Import Converter
+from pdf2docx import Converter
+import os
+
+# Set file path
+script_dir = os.path.dirname(os.path.abspath(__file__))
+pdf_file = os.path.join(script_dir, "MahidulIslam.pdf")
+output_file = os.path.join(script_dir, "MahidulIslam.docx")
+
+if not os.path.exists(pdf_file):
+    # print file not found message
+    print(f"File not found: {pdf_file}")
+elif os.path.exists(output_file):
+    # print file exist message
+    print(f"File already exist: {output_file}")
+else:
+    # Convert file
+    converter = Converter(pdf_file=pdf_file)
+    converter.convert(output_file)
+    converter.close()
